babf9d406e1775c09ac3774fb231476aa92e27d7
Igor Khokhriakov <Ingvord@users.noreply.github.com>
2016-02-26 20:16:25 +0300

    gweba: progress

--- controllers/tango_webapp/main_controller.js
+++ controllers/tango_webapp/main_controller.js
@@ -10,52 +10,30 @@ TangoWebapp.MainController = MVC.Controller.extend('main',{
     * @param {Object} params
     */
     load: function(params){
-        webix.proxy.DevicesTree = {
-            $proxy:true,
-            source:'http://localhost:8080/localhost/rest/rc2/devices',
-            load:function(view, callback){
-                //your loading pattern
-                alert("load");
-                var success = callback[0].success;
-                callback[0].success = function(text, xml , loader){
-                    alert("success");
-
-                    success(text, xml, loader);
-                }
-                webix.ajax(this.source, callback, view);
-            },
-            save:function(view, update, dp, callback){
-                //your saving pattern for single records
-                alert("save");
-                webix.ajax().post(url, data, callback);
-            },
-            saveAll:function(view, update, dp, callback){
-                //your saving pattern
-                alert("saveAll");
-                for (var i = 0; i < updates.length; i++) { alert("updates is probably undefined"); }
-            },
-            result:function(state, view, dp, text, data, loader){
-                //your logic of serverside response processing
-                alert("result");
-                dp.processResult(state, data, details);
-            }
-        }
-
-
-        var tree = {
-            view: "tree",
-            id:"myTree",
-            type:'lineTree',
-            data: []
-        };
-
+        //TODO jsTangORB must be migrated to ajax first
+        //TangoWebapp.db = new DataBase('http://localhost:8080/localhost/rest/rc2','sys/database/2');
+        //TODO ask user for rest_host
+        TangoWebapp.rest_api_url = 'http://localhost:8080/localhost/rest/rc2';
+        //draw ui
     	webix.ui({
             rows:[
                 { view:"template",
                     type:"header", template:"Tango Web Application" },
                 {
                     cols: [
-                        tree,
+                        {
+                            view: "tabview",
+                            cells: [
+                                {
+                                    header: "Device",
+                                    body: TangoWebapp.DeviceTreeConfig
+                        },
+                        {
+                            header: "Server",
+                            body: TangoWebapp.ServerTreeConfig
+                }
+            ]
+            },
                         {view:"resizer"},
                         {
                             view: "datatable",
@@ -67,6 +45,7 @@ TangoWebapp.MainController = MVC.Controller.extend('main',{
 
                     ]
                 },
+                {view:"resizer"},
                 {
                     view: "datatable",
                     autoConfig: true,
@@ -75,18 +54,16 @@ TangoWebapp.MainController = MVC.Controller.extend('main',{
             ]
         });
 
-        var treedata =  [{id:"root", value:"Cars", open:true, data:[
-            { id:"1", open:true, value:"Toyota", data:[
-                { id:"1.1", value:"Avalon" },
-                { id:"1.2", value:"Corolla" },
-                { id:"1.3", value:"Camry" }
-            ]},
-            { id:"2", value:"Skoda", open:true, data:[
-                { id:"2.1", value:"Octavia" },
-                { id:"2.2", value:"Superb" }
-            ]}
-        ]}];
 
-        $$('myTree').parse(treedata);
+        $$('device_tree').loadBranch(0, null, TangoWebapp.rest_api_url + '/devices/sys/database/2/commands/DbGetDeviceDomainList?input=*');
+
+        //webix.ajax().put(TangoWebapp.rest_api_url + '/devices/sys/database/2/commands/DbGetDeviceDomainList?input=*')
+        //    .then(function(response){
+        //        var data = response.json().output.map(function(el){ return {id:el, value:el, webix_kids:true}});
+        //        $$('device_tree').parse(data);
+        //    })
+        //    .fail(function(response){
+        //        webix.message('DbGetDeviceDomainList has failed!');
+        //    });
     }
 });
