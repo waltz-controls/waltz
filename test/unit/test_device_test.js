294cb0dac1f3732a4728703bf31d65cd51414caf
Igor Khokhriakov <Ingvord@users.noreply.github.com>
2016-03-27 02:06:59 +0300

    gweba: progress

--- test/unit/test_device_test.js
+++ test/unit/test_device_test.js
@@ -6,9 +6,10 @@ new MVC.Test.Unit('test_device',{
    test_info:function(){
       var device = new Device({url:"http://localhost:8080/localhost/rest/rc2/devices/sys/tg_test/1"});
 
-      device.info(this.next_callback("info_success"));
+      device.info();//fetches data
+      device.info().then(this.next_callback("info_success"));
    },
    info_success:function(resp){
-      this.assert(true);
+      this.assert_equal("sys/tg_test/1", resp.name);
    }
 });
