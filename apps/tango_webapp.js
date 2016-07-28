7328d58608189623521a39a4559767843068c645
Igor Khokhriakov <Ingvord@users.noreply.github.com>
2016-07-28 16:41:13 +0200

    progress: use include.libs to include webix

--- apps/tango_webapp.js
+++ apps/tango_webapp.js
@@ -1,6 +1,9 @@
 include.application('TangoWebapp','0.1-SNAPSHOT');
 include.css(
-    
+    "webix/codebase/skins/aircompact"
+);
+include.libs(
+    "webix/codebase/webix_debug"
 );
 include.resources(
     "tango_webapp/version",
@@ -23,21 +26,20 @@ include(function(){ //runs after prior includes are loaded
   include.views(
       "views/start_page", "views/device_info", "views/dev_panel_command_out", "views/dev_panel_attribute_info", "views/dev_panel_attribute_out", "views/dev_panel_pipe_out"
     );
+    include.resources(
+        //
+        "webix/device_tree", "webix/server_tree", "webix/toolbar",
+        //dataviews
+        "webix/device_info_view", "webix/device_properties_view", "webix/device_polling_view", "webix/device_event_view",
+        "webix/device_attribute_config_view", "webix/device_logging_view", "webix/device_pipe_config_view", "webix/device_attribute_properties_view",
+        //"webix/device_tab_view",
+        //plot
+        "webix/spectrum_plot","webix/image_plot",
+        //atk
+        "webix/atk_panel_view",
+        //windows
+        "webix/device_panel_window",
+        "webix/device_view",
+        "webix/main"
+    );
 });
-
-include.resources(
-    //
-    "webix/device_tree", "webix/server_tree", "webix/toolbar",
-    //dataviews
-    "webix/device_info_view", "webix/device_properties_view", "webix/device_polling_view", "webix/device_event_view",
-    "webix/device_attribute_config_view", "webix/device_logging_view", "webix/device_pipe_config_view", "webix/device_attribute_properties_view",
-    //"webix/device_tab_view",
-    //plot
-    "webix/spectrum_plot","webix/image_plot",
-    //atk
-    "webix/atk_panel_view",
-    //windows
-    "webix/device_panel_window",
-    "webix/device_view",
-    "webix/main"
-);
