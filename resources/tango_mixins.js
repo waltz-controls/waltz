bfd790a69200b73291a414e039158d6dc0293b95
Ingvord <tla-ingvord@yandex.ru>
2016-08-01 13:22:34 +0200

    progress: progress

--- resources/tango_mixins.js
+++ resources/tango_mixins.js
@@ -14,6 +14,28 @@ MVC.Object.extend(TangoWebapp, {
                 this._device = device;
                 return device;
             }
+        },
+
+        /**
+         * Performs action defined in run function only if this component is visible
+         */
+        Runnable: {
+            _delay: 1000,
+            _intervalId: 0,
+            start: function(){
+                this._intervalId = setInterval(function(){
+                    if(this.isVisible())
+                        this.run()
+                }.bind(this), this._delay);
+            },
+            changeDelay: function(delay){
+                this._delay = delay;
+                this.stop();
+                this.start();
+            },
+            stop:function(){
+                clearInterval(this._intervalId);
+            }
         }
     }
 });
