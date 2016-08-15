6dc733913e8b2a0dcf9403ca05b1ad6113f50b76
Igor Khokhriakov <Ingvord@users.noreply.github.com>
2016-08-15 12:45:21 +0200

    #22 progress

--- resources/tango_ui.js
+++ resources/tango_ui.js
@@ -21,6 +21,6 @@ if(MVC.Browser.Rhino)
     print("Skipping webix for Rhino");
 else
     if(MVC.env() === 'production') {
-        include.add( '../apps/tango_webapp/webix.js');//this file is created during compression
+        include.add( include.add_defaults('../apps/tango_webapp/webix')); //this file is created during compression
     }else
         include.apply(include, TangoWebapp.ui._webix_files);
