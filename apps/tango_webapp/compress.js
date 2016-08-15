85bc2efe45bea5854be8e184185317536af3ff27
Ingvord <tla-ingvord@yandex.ru>
2016-08-15 12:13:44 +0200

    #22 progress

--- apps/tango_webapp/compress.js
+++ apps/tango_webapp/compress.js
@@ -3,19 +3,26 @@
 MVCOptions = {
     onload: false,
     show_files: true,
-    compress_callback: function(total){
+    compress_callback: function (total) {
         var collected = MVCOptions.collect(total);
         MVCOptions.save('apps/tango_webapp/collected.js', collected);
         print("Compiler output start >>>>>>");
-        MVCOptions.compress('apps/tango_webapp/collected.js','apps/tango_webapp/production.js');
+        MVCOptions.compress('apps/tango_webapp/collected.js', 'apps/tango_webapp/production.js');
         print("<<<<<< Compiler output end.");
         MVCOptions.remove('apps/tango_webapp/collected.js');
         print("Compressed to 'apps/tango_webapp/production.js'.");
-        load('jmvc/rhino/documentation/setup.js');
-        
-        var app = new MVC.Doc.Application(total, "tango_webapp");
-        app.generate();
-        print("Generated docs.");
+
+        MVCOptions.compress.apply(null,
+            TangoWebapp.ui._webix_files.map(function (webix_file) {
+                return 'resources/' + webix_file + '.js';
+            }).concat(['apps/tango_webapp/webix.js']));
+
+        //load('jmvc/rhino/documentation/setup.js');
+        //
+        //var app = new MVC.Doc.Application(total, "tango_webapp");
+        //app.generate();
+        //print("Generated docs.");
+        print("Compressed to 'apps/tango_webapp/webix.js'.");
         quit();
     },
     env: "compress"
--- resources/tango_ui.js
+++ resources/tango_ui.js
@@ -1,18 +1,26 @@
-TangoWebapp.ui = {};
+TangoWebapp.ui = {
+    _webix_files: [
+        //
+        "webix/logger_view", "webix/device_tree", "webix/server_tree", "webix/toolbar",
+        //dataviews
+        "webix/device_info_view", "webix/device_properties_view", "webix/device_polling_view", "webix/device_event_view",
+        "webix/device_attribute_config_view", "webix/device_logging_view", "webix/device_pipe_config_view", "webix/device_attribute_properties_view",
+        //"webix/device_tab_view",
+        //plot
+        "webix/spectrum_plot","webix/image_plot",
+        //atk
+        "webix/atk_panel_view",
+        //windows
+        "webix/device_panel_window",
+        "webix/device_view",
+        "webix/main"
+    ]
+};
 
-include(
-    //
-    "webix/logger_view", "webix/device_tree", "webix/server_tree", "webix/toolbar",
-    //dataviews
-    "webix/device_info_view", "webix/device_properties_view", "webix/device_polling_view", "webix/device_event_view",
-    "webix/device_attribute_config_view", "webix/device_logging_view", "webix/device_pipe_config_view", "webix/device_attribute_properties_view",
-    //"webix/device_tab_view",
-    //plot
-    "webix/spectrum_plot","webix/image_plot",
-    //atk
-    "webix/atk_panel_view",
-    //windows
-    "webix/device_panel_window",
-    "webix/device_view",
-    "webix/main"
-);
+if(MVC.Browser.Rhino)
+    print("Skipping webix for Rhino");
+else
+    if(MVC.env() === 'production') {
+        include.add( '../apps/tango_webapp/webix.js');//this file is created during compression
+    }else
+        include.apply(include, TangoWebapp.ui._webix_files);
