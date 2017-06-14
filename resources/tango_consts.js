91c09263ce1db192ebff848f2699afee2b379123
ingvord <tla-ingvord@yandex.ru>
2017-06-14 12:47:22 +0300

    progress #39

--- resources/tango_consts.js
+++ resources/tango_consts.js
@@ -1,8 +1,8 @@
 TangoWebapp.consts = {
     REST_API_HOST: 'localhost',
-    REST_API_PORT: 8080,
+    REST_API_PORT: 10001,
     REST_API_VERSION: 'rc4',
-    TANGO_HOST: 'hzgxenvtest',
+    TANGO_HOST: 'localhost',
     TANGO_PORT: 10000,
     NAME_COLUMN_WIDTH: 250,
     TABS_DELIMITER_HEIGHT: 3,
