f593e36d9d287014a23b53ee7f36c19bcf3ce3f9
ingvord <tla-ingvord@yandex.ru>
2017-10-05 23:47:53 +0300

    add test TangoRestApi#fetchHost failed

--- test/unit/platform/tango_rest_api_test.js
+++ test/unit/platform/tango_rest_api_test.js
@@ -5,26 +5,27 @@ var test = new MVC.Test.Unit('tango_rest_api', {
 
         one.req_ids.push(1, 2, 3);
         this.assert_equal(0, two.req_ids.length);
-
-        this.assert_not(one.value == two.value);
-
-        two.value.add({id: 1});
-        this.assert(two.value.exists(1));
-        this.assert_not(one.value.exists(1));
     },
     test_send_event: function () {
-        var one = new TangoRestApi({url: 'http://localhost:10001'});
+        var one = new TangoRestApi({url: TestValues.rest_url});
         one.value.add({id: 1});
 
         one.setCursor(1)
     },
     test_load_host: function () {
-        var one = new TangoRestApi({url: 'http://localhost:10001'});
+        var one = new TangoRestApi({url: TestValues.rest_url});
 
-        one.fetchHost("localhost:10000").then(this.next_callback("check_load_host"))
+        one.fetchHost(TestValues.tango_host).then(this.next_callback("check_load_host"))
     },
     check_load_host: function (resp) {
         this.assert_equal("sys/database/2", resp.name)
-    }
+    },
+    test_load_host_failed: function () {
+        var one = new TangoRestApi({url: TestValues.rest_url});
 
+        one.fetchHost("xxx:10000").fail(this.next_callback("check_load_host_failed"))
+    },
+    check_load_host_failed: function (resp) {
+        this.assert_equal("unknown", resp.name)
+    }
 });
