003ecd7456fdca1e0e731545bb677bbeb1154c03
ingvord <mail@ingvord.ru>
2018-03-06 17:28:39 +0100

    Progress

--- apps/platform.js
+++ apps/platform.js
@@ -4,12 +4,20 @@
  */
 TangoWebappPlatform = {};
 
+REST_API_PROTOCOL = "http";
+REST_API_HOST = "localhost";
+REST_API_PORT = 10001;
+REST_API_VERSION = "rc4";
+TANGO_HOST = "localhost";
+TANGO_PORT = 10000;
+
 include(function () { //runs after prior includes are loaded
     include.plugins(
         "controller", "view", "model"
     );
 
     include.resources(
+        "constants",   //TODO if compress take ant output
         "platform/webix/tango_mixins",
         "platform/helpers"
     );
--- apps/tango_webapp.js
+++ apps/tango_webapp.js
@@ -2,26 +2,6 @@ include("platform");
 
 include.application('TangoWebapp', '0.3-rc9');
 
-/**
- * Useful (or not) constants
- *
- * @type {{REST_API_HOST: string, REST_API_PORT: number, REST_API_VERSION: string, TANGO_HOST: string, TANGO_PORT: number, NAME_COLUMN_WIDTH: number, TABS_DELIMITER_HEIGHT: number, DATABASE: string, LOG_DATE_FORMATTER: TangoWebapp.consts.LOG_DATE_FORMATTER}}
- */
-TangoWebapp.consts = {
-    REST_API_PROTOCOL: 'http',
-    REST_API_HOST: 'localhost',
-    REST_API_PORT: 10001,
-    REST_API_VERSION: 'rc4',
-    TANGO_HOST: 'localhost',
-    TANGO_PORT: 10000,
-    NAME_COLUMN_WIDTH: 250,
-    TABS_DELIMITER_HEIGHT: 3,
-    DATABASE: 'sys/database/2',
-    LOG_DATE_FORMATTER: function (date) {
-        return date;
-    }
-};
-
 include.css(
     "highlight"
     // "webix/codebase/skins/aircompact"
