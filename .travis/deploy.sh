e0a4273841c72ca38b02e1a23ef6cb33ea3f4680
ingvord <mail@ingvord.ru>
2018-03-07 12:26:51 +0100

    fix travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,6 +14,8 @@ export TANGO_PORT=10000;
 #decrypt
 openssl aes-256-cbc -K $encrypted_2f30ea6ab509_key -iv $encrypted_2f30ea6ab509_iv -in ./.travis/IngvordESRF.pem.enc -out ./.travis/IngvordESRF.pem -d
 
+ssh -o "StrictHostKeyChecking no" $REST_API_HOST
+
 scp -v -i ./.travis/IngvordESRF.pem build/distributions/TangoWebapp.war ubuntu@$REST_API_HOST:/home/ubuntu
 
 ssh -v -i ./.travis/IngvordESRF.pem ubuntu@$REST_API_HOST "sudo mv /home/ubuntu/TangoWebapp.war /var/lib/tomcat8/webapps"
