b2013d9e91989894320dc9e3c17b5598bf21003a
ingvord <mail@ingvord.ru>
2018-03-07 12:21:18 +0100

    fix travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-REST_API_PROTOCOL = "http";
-REST_API_HOST = "ec2-35-156-104-8.eu-central-1.compute.amazonaws.com";
-REST_API_PORT = 8080;
-REST_API_VERSION = "rc4";
-TANGO_HOST = "ip-172-31-30-179";
-TANGO_PORT = 10000;
+export REST_API_PROTOCOL="http";
+export REST_API_HOST="ec2-35-156-104-8.eu-central-1.compute.amazonaws.com";
+export REST_API_PORT=8080;
+export REST_API_VERSION="rc4";
+export TANGO_HOST="ip-172-31-30-179";
+export TANGO_PORT=10000;
 
 ./jmvcc jmvc/clean
 
@@ -14,6 +14,6 @@ TANGO_PORT = 10000;
 #decrypt
 openssl aes-256-cbc -K $encrypted_2f30ea6ab509_key -iv $encrypted_2f30ea6ab509_iv -in ./.travis/IngvordESRF.pem.enc -out ./.travis/IngvordESRF.pem -d
 
-scp -i ./.travis/IngvordESRF.pem build/distributions/TangoWebapp.war ubuntu@$REST_API_HOST:/home/ubuntu
+scp -v -i ./.travis/IngvordESRF.pem build/distributions/TangoWebapp.war ubuntu@$REST_API_HOST:/home/ubuntu
 
-ssh -i ./.travis/IngvordESRF.pem ubuntu@$REST_API_HOST "sudo mv /home/ubuntu/TangoWebapp.war /var/lib/tomcat8/webapps"
+ssh -v -i ./.travis/IngvordESRF.pem ubuntu@$REST_API_HOST "sudo mv /home/ubuntu/TangoWebapp.war /var/lib/tomcat8/webapps"
