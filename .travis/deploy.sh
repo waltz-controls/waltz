329abd442e5fd1a8374e33c4397668e60fc13b76
ingvord <mail@ingvord.ru>
2018-04-11 13:11:43 +0200

    enable deploy for every branch

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,6 +11,8 @@ export TANGO_PORT=10000;
 
 ./jmvcc jmvc/assemble
 
+mv build/distributions/TangoWebapp.war build/distributions/$TRAVIS_BRANCH.war
+
 #decrypt
 openssl aes-256-cbc -K $encrypted_2f30ea6ab509_key -iv $encrypted_2f30ea6ab509_iv -in ./.travis/IngvordESRF.pem.enc -out ./.travis/IngvordESRF.pem -d
 
@@ -18,6 +20,6 @@ chmod 600 ./.travis/IngvordESRF.pem
 
 ssh -o "StrictHostKeyChecking no" $REST_API_HOST
 
-scp -v -i ./.travis/IngvordESRF.pem build/distributions/TangoWebapp.war ubuntu@$REST_API_HOST:/home/ubuntu
+scp -v -i ./.travis/IngvordESRF.pem build/distributions/$TRAVIS_BRANCH.war ubuntu@$REST_API_HOST:/home/ubuntu
 
-ssh -v -i ./.travis/IngvordESRF.pem ubuntu@$REST_API_HOST "sudo mv /home/ubuntu/TangoWebapp.war /var/lib/tomcat8/webapps"
+ssh -v -i ./.travis/IngvordESRF.pem ubuntu@$REST_API_HOST "sudo mv /home/ubuntu/$TRAVIS_BRANCH.war /var/lib/tomcat8/webapps"
