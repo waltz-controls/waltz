82882ab4b7ffa254d5b0b419539584e66621c300
ingvord <mail@ingvord.ru>
2018-04-16 17:43:21 +0200

    Progress

--- resources/tango_webapp/attrs_monitor_view.js
+++ resources/tango_webapp/attrs_monitor_view.js
@@ -3,14 +3,75 @@
  */
 (function () {
     /**
+     *
+     * @return {} toolbar
+     */
+    newToolbar = function(){
+        return {
+            view: "toolbar",
+            height: 40,
+            cols: [
+                {view: "counter", id: "refresh", step: 100, value: 1000, min: 100, max: 100000, width: 90},
+                {
+                    view: "button",
+                    type: "iconButton",
+                    icon: "refresh",
+                    align: 'right',
+                    width: 30,
+                    click: function () {
+                        this.getTopParentView()._delay = this.getTopParentView().$$("refresh").getValue();
+                        if (this.getTopParentView().isRunning()) {
+                            this.getTopParentView().stop();
+                            this.getTopParentView().start();
+                        }
+                    }
+                },
+                {
+                    view: "toggle",
+                    id: "startStop",
+                    type: "iconButton",
+                    offIcon: "play",
+                    onIcon: "pause",
+                    align: 'right',
+                    width: 30,
+                    click: function () {
+                        if (this.getValue()) {
+                            this.getTopParentView().stop();
+                        } else {
+                            this.getTopParentView().start();
+                        }
+                    }
+                },
+                {}
+            ]
+        };
+    };
+
+    newScalarsPlot = function(){
+        return {
+            view: 'fieldset',
+            label: "Scalar plot",
+            body: TangoWebapp.ui.newScalarView({
+                id: 'plot',
+                empty: true
+            })
+        }
+    };
+
+
+    /**
      * @type {webix.protoUI}
      */
-    var _scalars = webix.protoUI({
+    var scalars = webix.protoUI({
         name: 'scalars',
+        _config: function () {
+            return {
+                }
+        },
         /**
          * @param {TangoAttribute} attr
          */
-        addAttribute:function(attr){
+        addAttribute: function (attr) {
             webix.assert(attr.info.data_format === 'SCALAR', "data_format must be SCALAR!");
 
             this.add({
@@ -23,19 +84,20 @@
             this.hideOverlay();
         },
         /**
-         * @param 
+         * @param
          */
-        update:function(attrs){
+        update: function (attrs) {
             this.parse(attrs);
         },
         $init: function (config) {
-            // webix.extend(config, this._ui());
+            webix.extend(config, this._config());
             this.$ready.push(function () {
                 this.showOverlay("No data...");
             }.bind(this));
         },
         defaults: {
             select: true,
+            resizeColumn: true,
             scheme: {
                 value: 'N/A',
                 quality: 'N/A',
@@ -74,7 +136,36 @@
                 {id: "description", header: "Description", fillspace: true}
             ]
         }
-    },webix.EventSystem, webix.OverlayBox, webix.ui.datatable);
+    }, webix.EventSystem, webix.OverlayBox, webix.ui.datatable);
+
+    newScalars = function(){
+        return {
+            view: 'tabview',
+            gravity: 2,
+            id: 'attributes',
+            cells: [
+                {
+                    header: "Scalars",
+                    body: {
+                        view: 'scalars',
+                        id: 'scalars',
+                        onClick: {
+                            "chart": function (event, id) {
+                                var attrId = id.row;
+                                var item = this.getItem(attrId);
+                                // this.getTopParentView().addTab(tabId, attrId, item);
+                                this.updateItem(attrId, {
+                                    plotted: !item.plotted
+                                });
+
+                                this.getTopParentView().handlePlot(item);
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    };
 
     /**
      * @type {webix.protoUI}
@@ -84,48 +175,75 @@
         _monitored: null,
         _plotted: null,
         _devices: null,
-        _add_attr:function(attr){
-            if(attr.info.data_format === 'SPECTRUM'){
+        _add_attr: function (attr) {
+            if (attr.info.data_format === 'SPECTRUM') {
                 this.$$('attributes').addView({
                     header: attr.info.label,
                     body: TangoWebapp.ui.newSpectrumView(attr)
                 });
-            } else if(attr.info.data_format === 'IMAGE'){
+            } else if (attr.info.data_format === 'IMAGE') {
                 this.$$('attributes').addView({
                     header: attr.info.label,
                     body: TangoWebapp.ui.newImageView(attr)
                 });
             }
         },
+        handlePlot: function (item) {
+            var $$plot = this.$$('plot');
+            if (item.plotted) {
+                $$plot.addTrace(item.label, [item.timestamp], [item.value], this._plotted.count() - 1);
+            } else {
+                var indexOf = this._plottedAttributes.indexOf(item);
+                $$plot.deleteTrace(indexOf);
+            }
+        },
         /**
          *
          */
-        update:function(attrs){
-            attrs.forEach(function(attr){
-                if(this.$$('attributes').getTabbar().getValue() === 'scalars'){
+        update: function (attrs) {
+            attrs.forEach(function (attr) {
+                if (this.$$('attributes').getTabbar().getValue() === 'scalars') {
                     this.$$('scalars').updateItem(attr.id, attr);
                 } else {
                     this.$$(attr.id).update(attr);
                 }
             }, this);
-            //TODO plotted
-        },
-        run:function(){
-            var attrs_to_update = [];
-            TangoWebappHelpers.iterate(this._plotted, function(plotted){
-                attrs_to_update.push(plotted);
-            });
 
-            if(this.$$('attributes').getTabbar().getValue() === 'scalars'){
-                TangoWebappHelpers.iterate(this.$$('scalars').data, function(scalar){
-                    if(!scalar.plotted) attrs_to_update.push(this._monitored.getItem(scalar.id));
+            if (this._plotted.count() === 0) return;
+
+            var plotted =[];
+            TangoWebappHelpers.iterate(this._plotted, function(scalar){
+                plotted.push(scalar);
+            });
+            this.$$('plot').updateTraces(
+                plotted.map(function (el, ndx) {
+                    return ndx;
+                }),
+                plotted.map(function (el) {
+                    return el.timestamp;
+                }),
+                plotted.map(function (el) {
+                    return el.value;
+                })
+                );
+        },
+        run: function () {
+            var attrs_to_update = [];
+            TangoWebappHelpers.iterate(this._plotted, function (plotted) {
+                attrs_to_update.push(
+                    this._monitored.getItem(plotted.id));
+            }.bind(this));
+
+            if (this.$$('attributes').getTabbar().getValue() === 'scalars') {
+                TangoWebappHelpers.iterate(this.$$('scalars').data, function (scalar) {
+                    if (!scalar.plotted) attrs_to_update.push(this._monitored.getItem(scalar.id));
                 }.bind(this));
             } else {
                 attrs_to_update.push(
                     this._monitored.getItem(this.$$('attributes').getTabbar().getValue()));
             }
 
-            for(var device_id in this._devices){
+            for (var device_id in this._devices) {
                 var device = PlatformContext.devices.getItem(device_id);
 
                 var filtered_attrs_to_update = attrs_to_update
@@ -135,11 +253,11 @@
                 device.fetchAttrValues(filtered_attrs_to_update
                     .map(function (attr_to_update) {
                         return attr_to_update.name;
-                    })).then(function(filtered_attrs_to_update, resp){
-                        this.update(filtered_attrs_to_update.map(function(filtered_attr_to_update, ndx){
-                            return MVC.Object.extend(filtered_attr_to_update, resp[ndx]);
-                        }));
-                    }.bind(this, filtered_attrs_to_update.slice()));
+                    })).then(function (filtered_attrs_to_update, resp) {
+                    this.update(filtered_attrs_to_update.map(function (filtered_attr_to_update, ndx) {
+                        return MVC.Object.extend(filtered_attr_to_update, resp[ndx]);
+                    }));
+                }.bind(this, filtered_attrs_to_update.slice()));
             }
         },
         /**
@@ -147,11 +265,11 @@
          * @param {TangoAttribute} attr
          */
         addAttribute: function (attr) {
-            if(this._monitored.getItem(attr.id) !== undefined) return;
+            if (this._monitored.getItem(attr.id) !== undefined) return;
 
             this._monitored.add(attr);
 
-            if(attr.info.data_format !== 'SCALAR'){
+            if (attr.info.data_format !== 'SCALAR') {
                 this._add_attr(attr);
             } else {
                 this.$$('scalars').addAttribute(attr);
@@ -159,64 +277,32 @@
 
             this._devices[attr.device_id] = null;
         },
-        $init:function(){
+        _ui: function () {
+            return {
+                rows: [
+                    newScalarsPlot(),
+                    {
+                        view: 'resizer'
+                    },
+                    newScalars(),
+                    newToolbar()
+                ]
+            }
+        },
+        $init: function (config) {
+            webix.extend(config, this._ui());
+
             this._devices = Object.create(null);
             this._monitored = new webix.DataCollection();
             this._plotted = new webix.DataCollection();
 
-            this.$ready.push(function(){
-                this._plotted.sync(this.$$('scalars').data, function(){
-                    this.filter(function(obj){
+            this.$ready.push(function () {
+                this._plotted.sync(this.$$('scalars').data, function () {
+                    this.filter(function (obj) {
                         return obj.plotted;
                     })
                 });
             }.bind(this));
-        },
-        defaults: {
-            rows:[
-                {
-                    template: 'plot'
-                },
-                {
-                    view: 'resizer'
-                },
-                {
-                    view: 'tabview',
-                    gravity: 2,
-                    id: 'attributes',
-                    cells: [
-                        {
-                            header: "Scalars",
-                            body: {
-                                view: 'scalars',
-                                id: 'scalars'
-                            }
-                        }
-                    ]
-                },
-                {
-                    view:"toolbar",
-                    height: 40,
-                    cols:[
-                        { view:"counter", id: "refresh", step:100, value:1000, min:100, max:100000, width: 90},
-                        { view:"button" , type:"iconButton", icon: "refresh", align: 'right', width: 30, click:function(){
-                            this.getTopParentView()._delay = this.getTopParentView().$$("refresh").getValue();
-                            if(this.getTopParentView().isRunning()) {
-                                this.getTopParentView().stop();
-                                this.getTopParentView().start();
-                            }
-                            }},
-                        { view:"toggle", id: "startStop", type:"iconButton", offIcon:"play",  onIcon:"pause", align: 'right', width: 30, click:function(){
-                            if(this.getValue()){
-                                this.getTopParentView().stop();
-                            } else {
-                                this.getTopParentView().start();
-                            }
-                            }},
-                        {}
-                    ]
-                }
-            ]
         }
     },
         TangoWebappPlatform.mixin.Runnable,
