bc961849d956a754638a841672eea73c0b7e17a2
ingvord <tla-ingvord@yandex.ru>
2018-06-26 11:58:54 +0300

    compress

--- apps/my_app/compress.js
+++ apps/my_app/compress.js
@@ -3,22 +3,24 @@
 MVCOptions = {
     onload: false,
     show_files: true,
-    compress_callback: function(total){
+    compress_callback: function (total) {
         var collected = MVCOptions.collect(total);
         MVCOptions.save('apps/my_app/collected.js', collected);
         print("Compiler output start >>>>>>");
-        MVCOptions.compress('apps/my_app/collected.js','apps/my_app/production.js');
+        MVCOptions.compress('apps/my_app/collected.js', 'apps/my_app/production.js');
         print("<<<<<< Compiler output end.");
         MVCOptions.remove('apps/my_app/collected.js');
         print("Compressed to 'apps/my_app/production.js'.");
-        load('jmvc/rhino/documentation/setup.js');
-        
-        var app = new MVC.Doc.Application(total, "my_app");
-        app.generate();
-        print("Generated docs.");
+
+        MVCOptions.compress.apply(null,
+            MyApp.ui._webix_files.map(function (webix_file) {
+                return 'resources/my_app/' + webix_file + '.js';
+            }).concat(['apps/my_app/webix.js']));
+
+        print("Compressed to 'apps/my_app/webix.js'.");
         quit();
     },
     env: "compress"
 }
 load('jmvc/rhino/compression/setup.js');
-window.location = 'apps/my_app/index.html';
+window.location = 'apps/my_app/compress.html'; //<!-- IMPORTANT
