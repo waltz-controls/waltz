6b6e18c8ed0555356483b68eee986b1f887ff55a
ingvord <tla-ingvord@yandex.ru>
2018-06-27 14:12:26 +0300

    custom css

---
+++ stylesheets/skin.js
@@ -0,0 +1,3 @@
+//[Skin Customization]
+webix.skin.material.barHeight=45;webix.skin.material.tabbarHeight=47;webix.skin.material.rowHeight=38;webix.skin.material.listItemHeight=34;webix.skin.material.inputHeight=38;webix.skin.material.layoutMargin.wide=10;webix.skin.material.layoutMargin.space=10;webix.skin.material.layoutPadding.space=10;
+ webix.skin.set('material')
