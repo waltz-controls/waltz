14698f9371a19fe9a7034d2d902a37a47d188075
ingvord <tla-ingvord@yandex.ru>
2019-05-26 20:52:20 +0300

    Close #189

--- resources/webix_widgets/search.js
+++ resources/webix_widgets/search.js
@@ -1,3 +1,33 @@
+const search2 = webix.protoUI({
+    name:"search2",
+    $cssName:"search",
+    $renderIcon:function(){
+        const config = this.config;
+
+            const height = config.aheight - 2*config.inputPadding,
+                padding = (height - 18)/2 -1;
+            let html = "", pos = 2;
+            const icons = ["times", "search"];
+            for(var i=0; i<icons.length; i++){
+                html+="<span style='right:"+pos+"px;height:"
+                    +(height-padding)+"px;padding-top:"+padding
+                    +"px;' class='webix_input_icon fa-"+icons[i]+"'></span>";
+
+                pos+=24;
+            }
+            return html;
+    },
+    on_click:{
+        "fa-search":function(e){
+            return this.callEvent("onFocus",[e]);
+        },
+        "fa-times":function(e){
+            this.setValue("");
+            return this.callEvent("onFocus",[e]);
+        }
+    },
+}, webix.ui.search);
+
 /**
  *
  * @param {string} target
@@ -5,7 +35,7 @@
  */
 export default function newSearch(target, filter){
     return {
-        view: "search",
+        view: "search2",
         placeholder: "type to filter",
         borderless: true,
         value: "",
@@ -26,7 +56,7 @@ export default function newSearch(target, filter){
  */
 export function newComplexSearch(filter){
     return {
-        view: "search",
+        view: "search2",
         id:"search",
         placeholder: "type to filter",
         borderless: true,
