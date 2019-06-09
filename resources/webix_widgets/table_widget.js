45958b443b62da778a8dd1105405ce9bf61a8e7d
ingvord <tla-ingvord@yandex.ru>
2019-06-09 20:42:36 +0300

    Progress: #197

--- resources/webix_widgets/table_widget.js
+++ resources/webix_widgets/table_widget.js
@@ -1,3 +1,5 @@
+const kPersistentColumns = ["id", "device", "remove"];
+
 export const TableWidgetController = class extends MVC.Controller {
     buildUI(platform_api) {
         platform_api.ui_builder.add_mainview_item(newTableWidgetTab());
@@ -29,9 +31,21 @@ const table_datatable = webix.protoUI({
         return {
             columns: [
                 {id: "id", hidden: true},
-                {id: "device", header: "Device", fillspace: true}
+                {id: "device", header: "Device", fillspace: true},
+                {
+                    id: "remove", header: "<span class='remove-all webix_icon fa-trash'></span>", width: 30,
+                    tooltip: "Remove all",
+                    template: function (obj) {
+                        return "<span class='remove-single webix_icon fa-trash'></span>";
+                    }
+                }
             ],
             on:{
+                onHeaderClick(obj){
+                    if(obj.column === 'remove'){
+                        this.clear();
+                    }
+                },
                 onItemClick(id) {
                     const device_id = id.row;
 
@@ -40,7 +54,8 @@ const table_datatable = webix.protoUI({
 
                     const attr_name = id.column;
 
-                    if(["id", "device"].includes(attr_name)) return;
+
+                    if(kPersistentColumns.includes(attr_name)) return;
 
                     OpenAjax.hub.publish("tango_webapp.item_selected", {
                         data: {
@@ -52,9 +67,19 @@ const table_datatable = webix.protoUI({
             }
         }
     },
+    clear(){
+        this.clearAll();
+        this._tracked_attrs = new Set([]);
+        this.removeColumns();
+    },
+    removeColumns(){
+        const columns = this.config.columns;
+        this.config.columns = columns.filter(column => kPersistentColumns.includes(column.id));
+        this.refreshColumns();
+    },
     addColumn(attr){
         const columns = this.config.columns;
-        columns.push({
+        columns.splice(columns.length - 1,0,{
             device_id: attr.device_id,
             id: attr.name,
             header: `${attr.display_name} (${attr.info.display_unit})`,
@@ -106,6 +131,27 @@ const table_datatable = webix.protoUI({
 
         this.run();
     },
+    async addDevice(id){
+        let device = TangoDevice.find_one(id);
+        if(device == null) {
+            try {
+                const parts = id.split('/');
+                const tango_host = parts.shift();
+                device = await PlatformContext.rest.fetchHost(tango_host)
+                    .then(host => host.fetchDevice(parts.join('/')))
+            } catch (e) {
+                TangoWebappHelpers.error(`Failed to fetch device[id=${id}]`,e);
+                return;
+            }
+        }
+
+        this.add({
+            id: device.id,
+            device: device.display_name,
+            _device: device,
+            _attrs:new Set(this._tracked_attrs)
+        });
+    },
     run(){
         this.data.each(item => {
             item._device.fetchAttrValues([...item._attrs]).then(resp => {
@@ -131,13 +177,16 @@ const table_datatable = webix.protoUI({
              * @see {@link https://docs.webix.com/api__dragitem_onbeforedrop_event.html| onBeforeDrop}
              */
             $drop:function(source, target){
-                var dnd = webix.DragControl.getContext();
-                if(dnd.from.config.$id !== 'attrs') return false;
+                const dnd = webix.DragControl.getContext();
+                if(dnd.from.config.$id === 'attrs') {
+                    const attr = TangoAttribute.find_one(dnd.source[0]);
+                    if (attr == null) return false;
 
-                var attr = TangoAttribute.find_one(dnd.source[0]);
-                if(attr == null) return false;
-
-                this.addAttribute(attr);
+                    this.addAttribute(attr);
+                }
+                if(dnd.from.config.view === 'devices_tree_tree'){
+                    this.addDevice(dnd.source[0]);
+                }
                 return false;
             }.bind(this)
         });
@@ -147,7 +196,14 @@ const table_datatable = webix.protoUI({
 function newTableWidgetTable(){
     return {
         id:"datatable",
-        view:"table_datatable"
+        view:"table_datatable",
+        onClick: {
+            "remove-single":function(event, id){
+                this.remove(id.row);
+                if(this.count() === 0) this.removeColumns();
+                return false;
+            }
+        }
     }
 }
 
@@ -156,10 +212,7 @@ const table_widget = webix.protoUI({
     _ui(){
         return {
             rows:[
-                newTableWidgetTable(),
-                {
-                    template:"table widget body"
-                }
+                newTableWidgetTable()
             ]
         }
     },
