9470e7fe9bb4f4b65f4a4d830e2cad240249b2ef
ingvord <mail@ingvord.ru>
2019-09-10 18:21:54 +0200

    enable XenvHQ widget

--- resources/webix_widgets/xenv.js
+++ resources/webix_widgets/xenv.js
@@ -80,7 +80,7 @@ export const XenvHqController = class extends MVC.Controller {
 };
 
 //disable Xenv widget for master
-// XenvHqController.initialize();
+XenvHqController.initialize();
 
 const xenvHq = webix.protoUI({
     name: "xenv-hq",
