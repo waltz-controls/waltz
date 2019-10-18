e4672863dc4f7d9c0e9a90db78cb2dae7159fd04
ingvord <mail@ingvord.ru>
2019-10-18 12:04:39 +0200

    fix travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,10 +3,10 @@
 #decrypt
 openssl aes-256-cbc -K $encrypted_password -in ./.travis/IngvordAWS.enc -out ./.travis/IngvordAWS.pem -d
 
-chmod 600 ./.travis/IngvordESRF.pem
+chmod 600 ./.travis/IngvordAWS.pem
 
 ssh -o "StrictHostKeyChecking no" $REST_API_HOST
 
-scp -v -i ./.travis/IngvordESRF.pem build/distributions/$TRAVIS_BRANCH.war ubuntu@$REST_API_HOST:/home/ubuntu
+scp -v -i ./.travis/IngvordAWS.pem build/distributions/$TRAVIS_BRANCH.war ubuntu@$REST_API_HOST:/home/ubuntu
 
-ssh -v -i ./.travis/IngvordESRF.pem ubuntu@$REST_API_HOST "sudo mv /home/ubuntu/$TRAVIS_BRANCH.war /var/lib/tomcat8/webapps"
+ssh -v -i ./.travis/IngvordAWS.pem ubuntu@$REST_API_HOST "sudo mv /home/ubuntu/$TRAVIS_BRANCH.war /var/lib/tomcat9/webapps"
