d793eec7845368b1e5a3303bca319a5e0aa3c3c2
ingvord <tla-ingvord@yandex.ru>
2019-10-19 05:08:27 +0300

    progress

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,7 @@
 
 #decrypt
 #openssl aes-256-cbc -pass pass:$super_secret_password -in ./.travis/IngvordAWS.pem.enc -out ./.travis/IngvordAWS.pem -d
-openssl aes-256-cbc -K $encrypted_c93bb15937a9_key -iv $encrypted_c93bb15937a9_iv -in IngvordAWS.pem.enc -out IngvordAWS.pem -d
+openssl aes-256-cbc -K $encrypted_c93bb15937a9_key -iv $encrypted_c93bb15937a9_iv -in ./.travis/IngvordAWS.pem.enc -out ./.travis/IngvordAWS.pem -d
 
 chmod 600 ./.travis/IngvordAWS.pem
 
