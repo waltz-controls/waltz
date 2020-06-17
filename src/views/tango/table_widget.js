waltz-controls/waltz

commit c323e64fe871d28b489259d4e0bc8dc82f83ebe4

Close #220

src/views/tango/table_widget.js
@@ -58,6 +58,16 @@ function getColumnConfig(attr){
     }, attr.isWritable() ? {editor:"text"} : {});
 }
 
+function getCommandColumnConfig(cmd){
+    return {
+        id: cmd.name,
+        header: { text:`${cmd.name} <span class="execute-all webix_icon mdi mdi-play-box-multiple-outline"></span>`, tooltip: "Execute All" },
+        template(obj){
+            return `${cmd.name} <span class="execute webix_icon mdi mdi-play-box-outline"></span>`
+        }
+    }
+}
+
 const table_datatable = webix.protoUI({
     name:"table_datatable",
     _config() {
@@ -66,6 +76,7 @@ const table_datatable = webix.protoUI({
             drag: true,
             resizeColumn: true,
             dragColumn: true,
+            tooltip: true,
             columns: [
                 {id: "id", hidden: true},
                 {id: "device", header: "Device", fillspace: true},
@@ -78,26 +89,7 @@ const table_datatable = webix.protoUI({
                         return kRemoveSingleHeader;
                     }
                 }
-            ],
-            on:{
-                onHeaderClick(obj){
-                    if(obj.column === 'remove'){
-                        this.config.root.clear();
-                    }
-                },
-                onItemClick(id) {
-                    const device_id = TangoId.fromDeviceId(id.row);
-
-                    this.config.root.dispatch(device_id,kActionSelectTangoDevice);
-                },
-                onAfterEditStop(value, editor) {
-                    if (value.value == value.old) return;
-
-                    const id = TangoId.fromMemberId(`${editor.row}/${editor.column}`);
-
-                    this.config.root.writeAttribute(id, value.value);
-                }
-            }
+            ]
         }
     },
     addColumns(attrs){
@@ -110,6 +102,14 @@ const table_datatable = webix.protoUI({
     },
     addColumn(attr){
         this.addColumns([attr])
+    },
+    addCommandColumn(cmd){
+        const columns = this.config.columns;
+
+        const config = getCommandColumnConfig(cmd)
+
+        columns.splice(columns.length - 1, 0, config);
+        this.refreshColumns();
     },
     /**
      *
@@ -127,6 +127,22 @@ const table_datatable = webix.protoUI({
                 [attr.name + "_quality"]: ""
             })
             }
+
+        this.run();
+    },
+
+    /**
+     *
+     * @param {TangoCommand} cmd
+     */
+    async addCommand(cmd){
+        if(this.config.columns.filter(column => column.id === cmd.name).length === 0)
+            this.addCommandColumn(cmd);
+
+        const item = this.getItem(cmd.tango_id.getTangoDeviceId());
+        if(item ===  undefined){
+            await this.config.root.addDevice(cmd.tango_id);
+        }
 
         this.run();
     },
@@ -204,9 +220,34 @@ function newTableWidgetTable(config) {
         root: config.root,
         stateId: config.id,
         view:"table_datatable",
+        on:{
+        onHeaderClick(obj){
+            if(obj.column === 'remove'){
+                this.config.root.clear();
+            }
+        },
+        onItemClick(id) {
+            const device_id = TangoId.fromDeviceId(id.row);
+
+            this.config.root.dispatch(device_id,kActionSelectTangoDevice);
+        },
+        onAfterEditStop(value, editor) {
+            if (value.value == value.old) return;
+
+            const id = TangoId.fromMemberId(`${editor.row}/${editor.column}`);
+
+            this.config.root.writeAttribute(id, value.value);
+        }
+    },
         onClick: {
             "remove-single":function(event, id){
                 this.config.root.removeDevice(TangoId.fromDeviceId(id.row));
+            },
+            "execute-all":function(event, id){
+                this.config.root.executeAll(id.column);
+            },
+            "execute":function(event, id){
+                this.config.root.execute(TangoId.fromMemberId(`${id.row}/${id.column}`));
             }
         }
     }

src/views/tango/remove_attribute_toolbar.js
@@ -55,6 +55,13 @@ const widget_settings = webix.protoUI({
     },
     /**
      *
+     * @param {TangoCommand} cmd
+     */
+    addCommand(cmd){
+        this.addAttribute(cmd);
+    },
+    /**
+     *
      * @param {TangoDevice} device
      */
     addDevice(device) {

src/widgets/tango/table_view.js
@@ -2,11 +2,16 @@ import "views/tango/table_widget";
 
 import {WaltzWidget} from "@waltz-controls/middleware";
 
-import {kTangoRestContext, TangoAttribute, TangoDevice} from "@waltz-controls/waltz-tango-rest-plugin";
+import {kTangoRestContext, TangoAttribute, TangoCommand, TangoDevice} from "@waltz-controls/waltz-tango-rest-plugin";
 import {kUserContext} from "@waltz-controls/waltz-user-context-plugin";
 import {kChannelLog, kTopicLog} from "controllers/log";
-import {kControllerUserAction, WriteTangoAttribute} from "@waltz-controls/waltz-user-actions-plugin";
+import {
+    ExecuteTangoCommand,
+    kControllerUserAction,
+    WriteTangoAttribute
+} from "@waltz-controls/waltz-user-actions-plugin";
 import {kControllerWebixMessage} from "controllers/message";
+import {TangoId} from "@waltz-controls/tango-rest-client";
 
 const kWidgetTableView = 'widget:table_view';
 const kFrozenOverlayMessage = "<span class='webix_icon mdi mdi-bell-ring'></span>This TableWidget's configuration is" +
@@ -25,6 +30,7 @@ class Context{
     constructor() {
         this.devices = [];
         this.attributes = [];
+        this.commands = [];
         this.frozen = false;
     }
 }
@@ -84,6 +90,8 @@ export default class TableViewWidget extends WaltzWidget {
 
         const attributes = newProxy.call(this,'attributes');
 
+        const commands = newProxy.call(this,'commands');
+
         this.devices = new webix.DataCollection({
             url: devices,
             save: devices
@@ -92,6 +100,11 @@ export default class TableViewWidget extends WaltzWidget {
             url: attributes,
             save: attributes
         });
+
+        this.commands = new webix.DataCollection({
+            url: commands,
+            save: commands
+        })
 
         this.restoreState();
     }
@@ -169,10 +182,17 @@ export default class TableViewWidget extends WaltzWidget {
         const attributes = userContext.get(this.id).attributes
             .map(attr => new TangoAttribute(attr))
             .filter((v, i, a) => a.findIndex(item => item.name === v.name) === i);
+        const commands = userContext.get(this.id).commands
+            .map(cmd => new TangoCommand(cmd))
+            .filter((v, i, a) => a.findIndex(item => item.name === v.name) === i);
         const devices = userContext.get(this.id).devices;
 
         this.$$datatable.addColumns(attributes);
+        commands.forEach(cmd => {
+            this.$$datatable.addCommand(cmd);
+        });
         this.$$settings.addAttributes(attributes);
+        this.$$settings.addAttributes(commands);
 
         this.$$datatable.parse(devices.map(device => ({...device, device: device.alias || device.name})));
 
@@ -193,7 +213,13 @@ export default class TableViewWidget extends WaltzWidget {
         this.$$datatable.removeAttribute(id.name);
         this.$$settings.removeAttribute(id.name);
 
-        this.attributes.remove(id.getTangoMemberId());
+        const tangoId = id.getTangoMemberId();
+
+        //TODO refactor, see #254
+        if(this.attributes.exists(tangoId))
+            this.attributes.remove(tangoId);
+        if(this.commands.exists(tangoId))
+            this.commands.remove(tangoId);
     }
 
     /**
@@ -285,7 +311,48 @@ export default class TableViewWidget extends WaltzWidget {
      *
      * @param {TangoId} id
      */
-    addCommand(id){
-        //TODO check if void -> add
+    async addCommand(id, force = false){
+        if(this.commands.exists(id.getTangoMemberId())){
+            this.app.getController(kControllerWebixMessage).show("Already added", 'info');
+            return;
+        }
+        if(this.frozen && !force) {
+            this.$$view.showOverlay(kFrozenOverlayMessage);
+            return;
+        }
+
+        const rest = await this.getTangoRest();
+
+        const cmd = await rest.newTangoCommand(id).toTangoRestApiRequest().get().toPromise().then(resp => new TangoCommand(resp));
+
+        if(!cmd.isVoid()) {
+            this.$$view.showOverlay("Only void commands are supported by this widget!");
+            return;
+        }
+
+        this.$$datatable.addCommand(cmd, force);
+        this.$$settings.addCommand(cmd, force);
+
+        this.commands.add(cmd);
+
+        this.$$view.run();
+    }
+
+    executeAll(name){
+        this.devices.data.each(device => {
+            this.execute(TangoId.fromDeviceId(device.id).setName(name))
+        })
+    }
+
+    /**
+     *
+     * @param {TangoId} id
+     */
+    async execute(id){
+        const rest = await this.getTangoRest();
+        const user = (await this.getUserContext()).user;
+        const command = await rest.newTangoCommand(id).toTangoRestApiRequest().get().toPromise().then(resp => new TangoCommand(resp));
+
+        this.app.getController(kControllerUserAction).submit(new ExecuteTangoCommand({user,command}))
     }
 }

commit 86f0aed491be098cf48b8c3110bf1ec85453706d

fix bug introduced in c323e64

src/views/tango/table_widget.js
@@ -89,7 +89,26 @@ const table_datatable = webix.protoUI({
                         return kRemoveSingleHeader;
                     }
                 }
-            ]
+            ],
+            on:{
+                onHeaderClick(obj){
+                    if(obj.column === 'remove'){
+                        this.config.root.clear();
+                    }
+                },
+                onItemClick(id) {
+                    const device_id = TangoId.fromDeviceId(id.row);
+
+                    this.config.root.dispatch(device_id,kActionSelectTangoDevice);
+                },
+                onAfterEditStop(value, editor) {
+                    if (value.value == value.old) return;
+
+                    const id = TangoId.fromMemberId(`${editor.row}/${editor.column}`);
+
+                    this.config.root.writeAttribute(id, value.value);
+                }
+            }
         }
     },
     addColumns(attrs){
@@ -220,25 +239,7 @@ function newTableWidgetTable(config) {
         root: config.root,
         stateId: config.id,
         view:"table_datatable",
-        on:{
-        onHeaderClick(obj){
-            if(obj.column === 'remove'){
-                this.config.root.clear();
-            }
-        },
-        onItemClick(id) {
-            const device_id = TangoId.fromDeviceId(id.row);
 
-            this.config.root.dispatch(device_id,kActionSelectTangoDevice);
-        },
-        onAfterEditStop(value, editor) {
-            if (value.value == value.old) return;
-
-            const id = TangoId.fromMemberId(`${editor.row}/${editor.column}`);
-
-            this.config.root.writeAttribute(id, value.value);
-        }
-    },
         onClick: {
             "remove-single":function(event, id){
                 this.config.root.removeDevice(TangoId.fromDeviceId(id.row));